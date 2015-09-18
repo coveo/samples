0773fc2be9aa759af93fe9be8583d8e74280c396
Yan Bussieres <ybussieres@coveo.com>
2015-09-18 15:52:47 -0400

    Script that does not kill CESService7
    With a pop up

---
+++ batch-file/ScriptThatDontKillCes.bat
@@ -0,0 +1,9 @@
+CALL procdump -ma CESService7.exe c:\ProcDumpFiles
+
+CALL procdump -ma CESCustomCrawlers7.exe c:\ProcDumpFiles
+
+CALL procdump -ma CESConverter7.exe c:\ProcDumpFiles
+
+
+
+CALL msg * "Please contact Coveo support, Too much memory is used by the service""
