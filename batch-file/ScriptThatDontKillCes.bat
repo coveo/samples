f6a89ebac453f95054607c16fe6bdf9ee06f360f
Yan Bussieres <ybussieres@coveo.com>
2015-11-25 10:35:34 -0500

    Update ScriptThatDontKillCes.bat
    added a command to delete the script after his execution

--- batch-file/ScriptThatDontKillCes.bat
+++ batch-file/ScriptThatDontKillCes.bat
@@ -7,3 +7,7 @@ CALL procdump -ma CESConverter7.exe c:\ProcDumpFiles
 
 
 CALL msg * "Please contact Coveo support, Too much memory is used by the service.""
+
+DEL "%~f0"
+
+REM Line 11 is to make sure the script deletes itself after beign executed to avoid multiple Dumps 
